b363be926105500ce37a7d1e5c653512747fd0c7
Diego n <diego.narvaez@gonet.us>
2019-06-18 17:58:45 -0500

    Se agrego menu al ViewPager

--- app/src/main/java/android/bignerdranch/practicaserviciorecycler/view/ViewPagerActivity.kt
+++ app/src/main/java/android/bignerdranch/practicaserviciorecycler/view/ViewPagerActivity.kt
@@ -2,6 +2,9 @@ package android.bignerdranch.practicaserviciorecycler.view
 
 import android.bignerdranch.practicaserviciorecycler.R
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentStatePagerAdapter
@@ -9,23 +12,39 @@ import androidx.viewpager.widget.ViewPager
 
 class ViewPagerActivity : AppCompatActivity() {
 
-    private lateinit var viewpager:ViewPager
+    private lateinit var viewpager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.view_pagert)
 
-        val fm= supportFragmentManager
-        viewpager= findViewById(R.id.viewpager)
-        viewpager.adapter= object : FragmentStatePagerAdapter(fm) {
+        val fm = supportFragmentManager
+        viewpager = findViewById(R.id.viewpager)
+        viewpager.adapter = object : FragmentStatePagerAdapter(fm) {
             override fun getItem(position: Int): Fragment {
-             return FragmentoPager.newInstance()
+                return FragmentoPager.newInstance()
             }
 
             override fun getCount(): Int {
                 return 3
             }
 
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater_menu=menuInflater
+        inflater_menu.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return if(item?.itemId==R.id.menu){
+            Toast.makeText(baseContext,"Este es un menu",Toast.LENGTH_SHORT).show()
+            true
+        }else{
+
+            super.onOptionsItemSelected(item)
         }
     }
 
